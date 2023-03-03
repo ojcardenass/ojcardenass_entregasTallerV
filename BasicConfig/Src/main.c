27266741ceedd4695e0ca6b53fb712922d05287b
Oscar Julián Cárdenas Sosa <ojcardenass@unal.edu.co>
2023-03-03 09:52:51 -0500

    Creacion BasicConfig
    Creacion del template BasicConfig

---
+++ BasicConfig/Src/main.c
@@ -0,0 +1,19 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Oscar Julián Cárdenas Sosa
+ * @brief          : Main program body
+ ******************************************************************************
+
+ */
+
+#include <stdint.h>
+
+int main(void)
+{
+    /* Loop forever */
+	while(1){
+
+	}
+	return 0;
+}
