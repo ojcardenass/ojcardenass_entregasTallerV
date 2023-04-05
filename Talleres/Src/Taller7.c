8b45a160fcb676a5c5ff90ae49f9e76844cdea4b
Oscar Julián Cárdenas Sosa <ojcardenass@unal.edu.co>
2023-04-05 12:30:37 -0500

    SolucionTarea3
    Se desarrolla la tarea #3, donde se implementan el ExtiDriver y el BasicTimer, con dos funciones en 7 segmentos. Y se corrige el nombre de la constante BTIMER_INTERRUPT ENABLE del BasicTimer

--- Talleres/Src/Taller7.c
+++ Talleres/Src/Taller7.c
@@ -19,34 +19,41 @@
 
 #include <stdint.h>
 
+uint8_t valorDato = 0;
+uint16_t casteoDato = 0;
+uint16_t *punteroDireccion = 0;
+uint32_t direccionDato = 0;
+uint8_t contenido = 0;
+
+
 int main(void){
-	// ejemplo 1
-	// Definimos varibles
-	uint8_t dato = 124;
-	// creamos un puntero con la direccion de memoria de la variable
-	uint8_t *pDato = &dato;
-
-	//Guardar el valor que esta en esa direccion de memoria
-	uint8_t valorDato = *pDato;
-
-	//Casteo basico
-	uint16_t casteoDato = (uint16_t) dato;
-
-	// Crear un puntero en una direccion especifica
-	uint16_t *punteroDireccion = (uint16_t *) 0x20000001;
-
-	// Cambiar direccion de memoria del puntero
-
-	punteroDireccion = (uint16_t *) 0x20000002;
-
-	// Guardar la direccion de memoria de un puntero
-	uint32_t direccionDato = (uint32_t) pDato;
-
-	// Cambiar el valor almacenado en el puntero
-	*pDato = 200;
-
-	// Aumentar 8 bits el puntero
-	pDato ++;
+//	// ejemplo 1
+//	// Definimos varibles
+//	uint8_t dato = 124;
+//	// creamos un puntero con la direccion de memoria de la variable
+//	uint8_t *pDato = &dato;
+//
+//	//Guardar el valor que esta en esa direccion de memoria
+//	uint8_t valorDato = *pDato;
+//
+//	//Casteo basico
+//	uint16_t casteoDato = (uint16_t) dato;
+//
+//	// Crear un puntero en una direccion especifica
+//	uint16_t *punteroDireccion = (uint16_t *) 0x20000001;
+//
+//	// Cambiar direccion de memoria del puntero
+//
+//	punteroDireccion = (uint16_t *) 0x20000002;
+//
+//	// Guardar la direccion de memoria de un puntero
+//	uint32_t direccionDato = (uint32_t) pDato;
+//
+//	// Cambiar el valor almacenado en el puntero
+//	*pDato = 200;
+//
+//	// Aumentar 8 bits el puntero
+//	pDato ++;
 
 	/* Arreglos
 	 * Listas de datos del mismo tipo
@@ -61,7 +68,7 @@ int main(void){
 	uint8_t miPrimerArreglo[sizeOfArray] = {5, 0xAE, 'a', 254};
 
 	// 2.1 Recorrer un arreglo con ciclos
-	uint8_t contenido = 0;
+//	uint8_t contenido = 0;
 
 	for(uint8_t i=0; i<sizeOfArray; i++){
 		contenido = miPrimerArreglo[i];
